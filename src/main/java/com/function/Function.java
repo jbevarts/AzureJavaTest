8057312eea84c2f6ceca7461c8a91c0f2f23e279
jbevarts <jbevarts@berkeley.edu>
2019-08-05 23:04:29 -0700

    updated dependencies in pom.xml, http trigger test now working, everything is green

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -1,49 +1,29 @@
 package com.function;
 
-import com.microsoft.azure.serverless.functions.annotation.*;
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
 
-import java.util.Map;
-
-import javax.tools.JavaFileObject;
-
-import com.microsoft.azure.serverless.functions.ExecutionContext;
-import com.microsoft.azure.serverless.functions.HttpRequestMessage;
-import com.microsoft.azure.serverless.functions.HttpResponseMessage;
-
-/**
- * Hello function with HTTP Trigger.
- */
 public class Function {
+    /**
+     * This function listens at endpoint "/api/hello". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/hello
+     * 2. curl {your host}/api/hello?name=HTTP%20Query
+     */
     @FunctionName("hello")
-    public HttpResponseMessage hello(@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage req,
-                        ExecutionContext context) {
-        
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = { HttpMethod.GET, HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        context.getLogger().info("hello was invoked");
-        Map<String, String> headers = req.getHeaders();
-        Object body = req.getBody();
-        String originText = "";
+        // Parse query parameter
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
 
-        String contentType = (String) headers.get("content-type");
-        
-        // how to check contentType
-        /*
-        switch (contentType) {
-            case "application/json":
-                //originText = getTextFromJSon((LinkedHashMap) body);
-                break;
-            case "text/plain":
-                originText = (String) body;
-                break;
-            default:
-                break;
-        }*/
-
-        //String jsonBody = createJSonMessage(originText,...);
-
-        HttpResponseMessage response = req.createResponse(200, "hello jerry");
-
-        return response;
-        
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
     }
 }
